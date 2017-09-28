3810231cdc60cd0afdbd0be304741b60973c8a8b
Chumu-J <chumu.jamal@gmail.com>
2017-09-28 22:06:32 +0200

    First commit recieved @Chuuchan!
    Added new code to sandbox.java
    -	Simple message confirming recievemnt of commit

---
+++ src/sandbox/Sandbox.java
@@ -0,0 +1,12 @@
+package sandbox;
+
+public class Sandbox {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Sandbox!");
+		System.out.println("Hello! ,Send my regards to the mac!");
+
+	}
+
+}
